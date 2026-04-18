e3375089c262213ce31744e20458a560c2073750
NARAYAN THAKARE <113063658+Narayan-Thakare@users.noreply.github.com>
2026-04-18 18:32:05 +0530

    Add: plus-one - submission-5

---
+++ Data Structures & Algorithms/plus-one/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] plusOne(int[] ar) {
+
+
+for(int i = ar.length-1 ; i>=0 ;i--){
+
+if(ar[i]<9){
+ar[i]=ar[i]+1;
+return ar;
+} 
+else ar[i]=0;
+
+}
+       
+int newar[] = new int [ar.length+1];
+newar[0]=1;
+return newar;
+
+        
+    }
+}
